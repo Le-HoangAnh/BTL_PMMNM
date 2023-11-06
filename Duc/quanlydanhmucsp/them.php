87cbb236f6c6fcfee616294b3d3267869cd08b49
Nguyen Duc <duc011002@gmail.com>
2023-11-06 22:44:55 +0700

    update code

---
+++ Duc/quanlydanhmucsp/them.php
@@ -0,0 +1,20 @@
+ <p class="title">Thêm danh mục sản phẩm</p>
+ <table width="50%" style="border-collapse: collapse;">
+   <form method="POST" action="modules/quanlydanhmucsp/xuly.php">
+    <tr>
+        <th style="font-size: 24px;" colspan="2">Điền danh mục sản phẩm</th>
+    </tr>
+    <tr>
+        <td>Tên danh mục</td>
+        <td><input type="text" name="tendanhmuc" ></td>
+    </tr>
+    <tr>
+        <td>Thứ tự</td>
+        <td><input type="text" name="thutu"></td>
+    </tr>
+    <tr>
+
+        <td colspan="2"><input type="submit" value="Thêm danh mục sản phẩm" name="themdanhmuc"></td>
+    </tr>
+</form>
+ </table>
